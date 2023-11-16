530a588fcec4405043eddf4d88539a52ca649bf5
n0rb92 <98477543+n0rb92@users.noreply.github.com>
2023-11-16 13:21:44 +0100

    Separate UI from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:norbert_cholody_cv/cubit/theme_cubit.dart';
-import 'package:norbert_cholody_cv/pages/my_story_page.dart';
-import 'package:norbert_cholody_cv/widgets/wave_widget.dart';
+import 'package:norbert_cholody_cv/pages/first_page.dart';
 
 void main() {
   runApp(
@@ -27,115 +26,9 @@ class MyCVApp extends StatelessWidget {
         return MaterialApp(
           title: 'Norbert Chołody CV',
           theme: theme,
-          home: const CVPage(),
+          home: const FirstPage(),
         );
       },
-    );
-  }
-}
-
-class CVPage extends StatelessWidget {
-  const CVPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final bool keyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: const Text(
-          'Norbert Chołody curriculum vitae',
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 40),
-        ),
-        centerTitle: true,
-      ),
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.easeOutQuad,
-            top: keyboardOpen ? size.height / 10 : size.height / 5,
-            child: WaveWidget(
-              size: size,
-              yOffset: size.height / 4,
-              color: Colors.white,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(50),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('images/foto.jpg'),
-              maxRadius: 150,
-            ),
-          ),
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 300),
-            decoration: BoxDecoration(
-              color: keyboardOpen ? Colors.white : Colors.transparent,
-            ),
-            width: double.infinity,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const SizedBox(height: 500),
-                const Text(
-                  'CV Language:',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                      letterSpacing: 1),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (context) => const MyStoryPage()),
-                            );
-                          },
-                          child: const Text(
-                            'English',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                letterSpacing: 1),
-                          )),
-                      const SizedBox(width: 20),
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (context) => const MyStoryPage()),
-                            );
-                          },
-                          child: const Text(
-                            'Polish',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                letterSpacing: 1),
-                          ))
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
